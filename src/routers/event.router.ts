fdbfacc3f62d4bf38ea8fa4e8c72fab220e8da61
Muhammad Wildan Sapoetro <muhwilsap@gmail.com>
2024-12-13 15:09:02 +0700

    Event api -  add field image in model event (#1)
    * add get events function
    
    * add field image in model event

---
+++ src/routers/event.router.ts
@@ -0,0 +1,21 @@
+import { Router } from "express";
+import { EventController } from "../controllers/event.controller";
+
+export class EventRouter {
+  private eventController: EventController;
+  private router: Router;
+
+  constructor() {
+    this.eventController = new EventController();
+    this.router = Router();
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes() {
+    this.router.get("/", this.eventController.getEvents);
+  }
+
+  getRouter(): Router {
+    return this.router;
+  }
+}
