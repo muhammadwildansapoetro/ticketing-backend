635ae6a2599029ec37207b94bf517e301367033a
muhammadwildansapoetro <muhwilsap@gmail.com>
2024-12-28 11:57:05 +0700

    merge

--- src/controllers/event.controller.ts
+++ src/controllers/event.controller.ts
@@ -2,11 +2,14 @@ import { Request, Response } from "express";
 import { cloudinaryUpload } from "../services/cloudinary";
 import prisma from "../prisma";
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { EventCategory, Prisma } from "prisma/generated/client";
 import { Readable } from "stream";
 =======
 import { cloudinaryUpload } from "../services/cloudinary";
 >>>>>>> e8fdcc7 (prepare deploy (#2))
+=======
+>>>>>>> 35791ec (update)
 
 export class EventController {
   async createEvent(req: Request, res: Response) {
